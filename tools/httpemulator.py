7a26419916c6f2535784dfc0d56f8b5705136b1a
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-03-28 09:08:48 +0100

    Update http emulator to respond to any request from IOC

--- tools/httpemulator.py
+++ tools/httpemulator.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/<anything>/<anything2>/<anything3>/<anything4>/<anything5>/<anything6>", methods=["GET", "POST", "PUT"])
+@app.route("/<anything>/<anything2>/<anything3>/<anything4>/<anything5>", methods=["GET", "POST", "PUT"])
+@app.route("/<anything>/<anything2>/<anything3>/<anything4>", methods=["GET", "POST", "PUT"])
+@app.route("/<anything>/<anything2>/<anything3>", methods=["GET", "POST", "PUT"])
+@app.route("/<anything>/<anything2>", methods=["GET", "POST", "PUT"])
+@app.route("/<anything>", methods=["GET", "POST", "PUT"])
+def handle_any_request(*args, **kwargs):
+    print("\n")
+    print(request)
+    if request.json:
+        print(request.json)
     return "Hello, IOC! I am an Eiger detector, promise."
 
 
