31e835375f0ef62742551558797339440ded98a0
J. Lewis Muir <jlmuir@imca-cat.org>
2020-07-08 20:55:00 -0500

    Only fetch mDCUBufFree parameter for EIGER1
    The mDCUBufFree parameter is only created in the eigerDetector
    constructor if mEigerModel is EIGER1, but the mDCUBufFree parameter is
    fetched unconditionally later in the constructor which will result in
    a crash if mEigerModel is not EIGER1.  Fix this by only fetching the
    mDCUBufFree parameter if mEigerModel is EIGER1.

--- eigerApp/src/eigerDetector.cpp
+++ eigerApp/src/eigerDetector.cpp
@@ -1722,10 +1722,10 @@ asynStatus eigerDetector::eigerStatus (void)
             status |= mLink2->fetch();
             status |= mLink3->fetch();
         }
-    }
 
-    // Read DCU buffer free percentage
-    status |= mDCUBufFree->fetch();
+        // Read DCU buffer free percentage
+        status |= mDCUBufFree->fetch();
+    }
 
     // Read state of the different modules
     status |= mState->fetch();
