2723ba1f6fa07825fca2e5ffb0e775318db35b25
Mark Rivers <rivers@cars.uchicago.edu>
2025-05-14 13:13:25 -0500

    Fixed decompressing LZ4; added support for multiple thresholds

--- eigerApp/src/stream2Api.cpp
+++ eigerApp/src/stream2Api.cpp
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <lz4.h>
 #include <bitshuffle.h>
+#include <compression.h>
 
 #define ZMQ_PORT        31001
 
@@ -19,31 +20,42 @@
 
 using std::string;
 
-static int uncompress (char *pInput, char *dest, char *encoding, size_t uncompressedSize, NDDataType_t dataType)
+static int uncompress (const unsigned char *pInput, char *dest, char *encoding, 
+                       size_t compressedSize, size_t uncompressedSize, NDDataType_t dataType)
 {
     const char *functionName = "uncompress";
-
+    size_t elemSize;
+    uint64_t orig_size = *(uint64_t *)pInput;
+    orig_size = be64toh(orig_size);
+    uint32_t block_size = *(uint32_t *)(pInput + 8);
+    block_size = be32toh(block_size);
+    switch (dataType) 
+    {
+        case NDUInt32: elemSize=4; break;
+        case NDUInt16: elemSize=2; break;
+        case NDUInt8:  elemSize=1; break;
+        default:
+            ERR_ARGS("unknown dataType=%d", dataType);
+            return STREAM_ERROR;
+    }
     if (strcmp(encoding, "lz4") == 0) {
-        ERR("calling LZ4_decompress_fast");
-        int result = LZ4_decompress_fast(pInput, dest, (int)uncompressedSize);
+//        ERR("calling LZ4_decompress_fast");
+//        size_t result = LZ4_decompress_fast((const char *)pInput+16, dest, (int)uncompressedSize);
+        size_t result = compression_decompress_buffer(COMPRESSION_LZ4,
+                                                      dest,
+                                                      uncompressedSize,
+                                                      (const char *)pInput,
+                                                      compressedSize,
+                                                      elemSize);        
         if (result < 0)
         {
-            ERR_ARGS("LZ4_decompress failed, result=%d\n", result);
+            ERR_ARGS("LZ4_decompress failed, result=%lu\n", result);
             return STREAM_ERROR; 
         }
     } 
     else if (strcmp(encoding, "bslz4") == 0)  {
         pInput += 12;   // compressed sdata is 12 bytes into buffer
-        size_t elemSize;
-        switch (dataType) 
-        {
-            case NDUInt32: elemSize=4; break;
-            case NDUInt16: elemSize=2; break;
-            case NDUInt8:  elemSize=1; break;
-            default:
-                ERR_ARGS("unknown frame type=%d", dataType);
-                return STREAM_ERROR;
-        }
+
         size_t numElements = uncompressedSize/elemSize;
         int result = bshuf_decompress_lz4(pInput, dest, numElements, elemSize, 0);
         if (result < 0)
@@ -119,7 +131,6 @@ int Stream2API::getHeader (stream_header_t *header, int timeout)
     }
     stream2_start_msg* sm = (stream2_start_msg *)s2msg;
 
-    ERR_ARGS("sm->type=%d", sm->type);
     if (sm->type != STREAM2_MSG_START) {
         ERR_ARGS("unexpected message type, should be STREAM2_MSG_START (%d), actual=%d", STREAM2_MSG_START, sm->type);
         return STREAM_ERROR;
@@ -129,18 +140,15 @@ int Stream2API::getHeader (stream_header_t *header, int timeout)
     mImage_size_x = sm->image_size_x;
     mImage_size_y = sm->image_size_y;
     mNumber_of_images = sm->number_of_images;
-    ERR_ARGS("series_id=%ld, image_dtype=%s, image_size_x=%ld, image_size_y=%ld, number_of_images=%ld",
-             sm->series_id, sm->image_dtype, sm->image_size_x, sm->image_size_y, sm->number_of_images); 
     return STREAM_SUCCESS;
 }
 
 int Stream2API::waitFrame (int *end, int timeout)
 {
-    const char *functionName = "waitFrame";
+    //const char *functionName = "waitFrame";
     int err = STREAM_SUCCESS;
     *end = false;
 
-    ERR("entry");
     if(timeout && (err = poll(timeout)))
         return err;
 
@@ -154,7 +162,6 @@ int Stream2API::waitFrame (int *end, int timeout)
     }
     mImageMsg = (stream2_image_msg *)s2msg;
 
-    ERR_ARGS("im->type=%d", mImageMsg->type);
     if (mImageMsg->type == STREAM2_MSG_END)
     {
         *end = true;
@@ -167,27 +174,35 @@ int Stream2API::getFrame (NDArray **pArrayOut, NDArrayPool *pNDArrayPool, int de
     const char *functionName = "getFrame";
     int err = STREAM_SUCCESS;
     size_t compressedSize, uncompressedSize;
-    size_t dims[2];
+    size_t dims[3];
+    int numDims;
     NDArray *pArray;
     char encoding[32];
     NDDataType_t dataType;
+    int numThresholds = mImageMsg->data.len;
 
-    ERR_ARGS("mImageMsg->type=%d", mImageMsg->type);
     switch (mImageMsg->type) {
        case STREAM2_MSG_IMAGE:
-            for (size_t i = 0; i < mImageMsg->data.len; i++) {
+            for (int i = 0; i < numThresholds; i++) {
                 struct stream2_image_data *pSID = &mImageMsg->data.ptr[i];
-                printf("data: \"%s\" ", pSID->channel);
                 struct stream2_multidim_array mda = pSID->data;
-                dims[1] = mda.dim[0];
                 dims[0] = mda.dim[1];
+                dims[1] = mda.dim[0];
+                numDims = 2;
+                if (numThresholds > 1) {
+                    dims[2] = numThresholds;
+                    numDims = 3;
+                }
                 struct stream2_typed_array *pS2Array = &mda.array;
                 stream2_typed_array_tag s2DataType = (stream2_typed_array_tag)pS2Array->tag;
                 struct stream2_bytes *pSB = &pS2Array->data;
                 compressedSize = pSB->len;
+                uncompressedSize = pSB->len;
                 struct stream2_compression *pCompression = &pSB->compression;
-                uncompressedSize = pCompression->orig_size;
-                if (pCompression->algorithm != NULL) strcpy(encoding, pCompression->algorithm);
+                if (pCompression->algorithm != NULL) {
+                    uncompressedSize = pCompression->orig_size;
+                    strcpy(encoding, pCompression->algorithm);
+                }
                 switch (s2DataType) {
                     case STREAM2_TYPED_ARRAY_UINT8:
                         dataType = NDUInt8;
@@ -204,28 +219,31 @@ int Stream2API::getFrame (NDArray **pArrayOut, NDArrayPool *pNDArrayPool, int de
                         goto error;
                 }
                 
-                if(!(pArray = pNDArrayPool->alloc(2, dims, dataType, 0, NULL)))
-                {
-                    ERR("failed to allocate NDArray for frame");
-                    err = STREAM_ERROR;
-                    goto error;
+                // On first threshold we create the NDArray
+                if (i == 0) {
+                    if(!(pArray = pNDArrayPool->alloc(numDims, dims, dataType, 0, NULL)))
+                    {
+                        ERR("failed to allocate NDArray for frame");
+                        err = STREAM_ERROR;
+                        goto error;
+                    }
                 }
                 // Get frame data
                 // If data is uncompressed we can copy directly into NDArray
                 if (pCompression->algorithm == NULL)
                 {
-                    memcpy(pArray->pData, pSB->ptr, compressedSize);
+                    memcpy((char *)pArray->pData + (i*uncompressedSize), pSB->ptr, uncompressedSize);
                 }
                 else 
                 {
-                    char *temp = (char *)malloc(compressedSize);
                     if (decompress) 
                     {
-                        uncompress(temp, (char *)pArray->pData, encoding, uncompressedSize, dataType);
+                        uncompress(pSB->ptr, (char *)pArray->pData + (i*uncompressedSize), encoding, 
+                                   compressedSize, uncompressedSize, dataType);
                     }
                     else
                     {        
-                        char *pInput = temp;
+                        const unsigned char *pInput = pSB->ptr;
                         if (strcmp(encoding, "lz4") == 0) 
                         {
                             pArray->codec.name = "lz4";
@@ -242,7 +260,6 @@ int Stream2API::getFrame (NDArray **pArrayOut, NDArrayPool *pNDArrayPool, int de
                         pArray->compressedSize = compressedSize;
                         memcpy(pArray->pData, pInput, compressedSize);
                     }
-                    free(temp);
                 }
                 *pArrayOut = pArray;
             }
